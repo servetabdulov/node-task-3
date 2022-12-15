a18a6f8f80d4a2d1b8d1ffe5e1db7362aba38d80
servetabdulov <servet.abdulov.2015@gmail.com>
2022-12-15 14:59:41 +0400

    add

---
+++ index.js
@@ -0,0 +1,19 @@
+const fs = require("fs");
+const express = require("express");
+const app = express();
+
+fs.readFile("./data.json", "utf8", (err, data) => {
+  if (!err) {
+    app.get("/", function (req, res) {
+      res.send("Datanı görmək üçün '/get' yazın");
+    });
+    app.get("/get", function (req, res) {
+      res.send(data);
+    });
+    app.listen(3000, function () {
+      console.log("Example app listening on port 3000!");
+    });
+  } else {
+    console.error(err);
+  }
+});
